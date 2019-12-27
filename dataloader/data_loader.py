a4908647b890d736299a830a67642e7b3f8f094b
idealwhite <814029010@qq.com>
2019-12-27 22:32:27 +0800

    add config of place2 and remove category json's usage

--- dataloader/data_loader.py
+++ dataloader/data_loader.py
@@ -33,10 +33,6 @@ class CreateDataset(data.Dataset):
         # load caption file
         with open(text_config.CAPTION, 'r') as f:
             self.captions = json.load(f)
-        with open(text_config.CATE_IMAGE_TRAIN, 'r') as f:
-            self.category_images_train = json.load(f)
-        with open(text_config.IMAGE_CATE_TRAIN, 'r') as f:
-            self.images_category = json.load(f)
 
         x = pickle.load(open(text_config.VOCAB, 'rb'))
         self.ixtoword = x[2]
@@ -73,24 +69,6 @@ class CreateDataset(data.Dataset):
         img_name = os.path.basename(img_name)
         captions = self.captions[img_name]
         caption = captions[caption_index_of_image] if type(captions) == list else captions
-        # if self.opt.isTrain:
-        #     image_category = self.images_category[img_name]
-        #     # get negative category
-        #     alternate_category = list(self.category_images_train.keys())
-        #     alternate_category.remove(image_category)
-        #     negative_category_id = random.randint(0, len(alternate_category)-1)
-        #     negative_category = alternate_category[negative_category_id]
-        #     # get negative image
-        #     negative_image_id = random.randint(0, len(self.category_images_train[negative_category])-1)
-        #     negative_image = self.category_images_train[negative_category][negative_image_id]
-        #     # get negative caption
-        #     negative_captions = self.captions[negative_image]
-        #     negative_caption = negative_captions[random.randint(0,len(negative_captions)-1)] \
-        #                                         if type(negative_captions) == list else negative_captions
-        #     negative_caption_idx, negative_caption_len = util._caption_to_idx(\
-        #                                         self.wordtoix, negative_caption, self.max_length)
-        # else:
-        #     negative_caption_idx, negative_caption_len = None, None
         caption_idx, caption_len = util._caption_to_idx(self.wordtoix, caption, self.max_length)
 
         return caption_idx, caption_len, caption, img_name
