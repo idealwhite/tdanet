544fc45e57a4f697b1e7c2c224641e57caa567d7
lisai <lisaizhang@foxmail.com>
2020-09-10 15:40:01 +0800

    change annote for save options

--- options/train_options.py
+++ options/train_options.py
@@ -19,8 +19,8 @@ class TrainOptions(BaseOptions):
         # display the results
         parser.add_argument('--display_freq', type=int, default=100, help='frequency of showing training results on screen')
         parser.add_argument('--print_freq', type=int, default=100, help='frequency of showing training results on console')
-        parser.add_argument('--save_latest_freq', type=int, default=1000, help='frequency of saving the latest results')
-        parser.add_argument('--save_iters_freq', type=int, default=10000, help='frequency of saving checkpoints at the end of epochs')
+        parser.add_argument('--save_latest_freq', type=int, default=1000, help='frequency of saving the latest results at each batch')
+        parser.add_argument('--save_iters_freq', type=int, default=10000, help='frequency of saving checkpoints at the end of batch')
         parser.add_argument('--no_html', action='store_true', help='do not save intermediate training results')
 
         self.isTrain = True
